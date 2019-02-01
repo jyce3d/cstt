3fec5e62637b3a52be941c472cf99a5b6592a0d9
jyce3d <42608645+jyce3d@users.noreply.github.com>
2019-02-01 16:41:28 +0100

    Update RootConfig.java

--- src/be/sdlg/webapps/config/RootConfig.java
+++ src/be/sdlg/webapps/config/RootConfig.java
@@ -25,7 +25,7 @@ public class RootConfig {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/stt_db");
         dataSource.setUsername("root");
-        dataSource.setPassword("123niab");
+        dataSource.setPassword("secret");
         return dataSource;
     }
 	
